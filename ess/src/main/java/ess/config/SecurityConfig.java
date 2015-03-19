f2fe04ab93259ebcfe33c6404df6d06b99867c1f
Det Buaklee <dbuaklee@hotmail.com>
2015-03-19 17:58:04 +0700

    initial commit

---
+++ ess/src/main/java/ess/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package ess.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER");
+    }
+	
+	
+	protected void configure(HttpSecurity http) throws Exception {
+	    http
+			.authorizeRequests()
+				.antMatchers("/static/**").permitAll()
+				.antMatchers("/webjars/**").permitAll()
+				.antMatchers("/landing-login**").permitAll()
+				.antMatchers("/web/**").permitAll()
+				.anyRequest().fullyAuthenticated()
+				.and()
+			.formLogin()
+				.loginPage("/landing-login").permitAll()
+				.loginProcessingUrl("/login")
+				.defaultSuccessUrl("/")
+				.and()
+			.logout().logoutUrl("/logout");
+			
+	}
+	
+}
---
+++ ess/src/main/java/ess/controller/MainController.java
@@ -0,0 +1,20 @@
+package ess.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MainController {
+	private Log log = LogFactory.getLog(this.getClass());
+	
+	@RequestMapping("/web/")
+	public String index(Model model) {
+		log.debug("Entering : /");
+		
+		return "dashboard";
+	}
+}
